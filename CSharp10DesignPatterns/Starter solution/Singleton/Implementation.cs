429f7a2b8a8d2b4df9de63080bcf47fb3291b6d1
Brian Andersen <brianmanden@gmail.com>
2024-03-04 10:29:19 +0100

    Implementing Logger Testing Logger (not thread safe)

--- CSharp10DesignPatterns/Starter solution/Singleton/Implementation.cs
+++ CSharp10DesignPatterns/Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,40 @@
 ﻿namespace Singleton
 {
-  
+	/// <summary>
+	/// Singleton
+	/// https://app.pluralsight.com/library/courses/c-sharp-10-design-patterns/table-of-contents > Creational Pattern: Singleton
+	/// </summary>
+	public class Logger
+	{
+		private static Logger? _instance;
+
+		public static Logger Instance
+		{
+			get
+			{
+				//lazy instatiation
+				if (_instance is null)
+				{
+					_instance = new Logger();
+				}
+
+				return _instance;
+			}
+		}
+
+		// Protected Constructor so the class cannot be instantiated but it can be subclassed.
+		protected Logger()
+		{
+
+		}
+
+		/// <summary>
+		/// Singleton Operation
+		/// </summary>
+		/// <param name="message"></param>
+		public void Log(string message)
+		{
+			Console.WriteLine($"Message to log: {message}");
+		}
+	}
 }
