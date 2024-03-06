5aaa52f8ac6fdbaca8ea440f05e87e466b5b8e24
Brian Andersen <brianmanden@gmail.com>
2024-03-06 11:18:08 +0100

    Implement FactoryMethod Pattern

--- CSharp10DesignPatterns/Starter solution/FactoryMethod/Program.cs
+++ CSharp10DesignPatterns/Starter solution/FactoryMethod/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+string countryIdentifier = "BE";
+
+List<DiscountFactory> factories = new List<DiscountFactory>{
+	new CodeDiscountFactory(Guid.NewGuid()),
+	new CountryDiscountFactory( countryIdentifier),
+};
+
+foreach (DiscountFactory factory in factories)
+{
+	var discountService = factory.CreateDiscountService();
+
+	Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
+}
+
+Console.WriteLine("Press any key to terminate program.");
+Console.ReadKey();
