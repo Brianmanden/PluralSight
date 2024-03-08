56f85e426ab467012a8ae08a2a33826aef82c073
Brian Andersen <brianmanden@gmail.com>
2024-03-08 10:16:26 +0100

    Implementing Builder Pattern

--- CSharp10DesignPatterns/Starter solution/Builder/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Builder/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+Garage garage = new();
+
+MiniBuilder miniBuilder = new MiniBuilder();
+BMWBuilder BMWBuilder = new BMWBuilder();
+
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+// or
+//garage.Show();
+
+garage.Construct(BMWBuilder);
+//Console.WriteLine(BMWBuilder.Car.ToString());
+// or
+garage.Show();
+
+Console.WriteLine("Press any key to terminate program.");
+Console.ReadKey();
