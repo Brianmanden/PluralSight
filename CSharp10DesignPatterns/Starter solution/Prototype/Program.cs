8697fc7a046f706e893d10ac87e1a0d58d532982
Brian Andersen <brianmanden@gmail.com>
2024-03-09 08:25:51 +0100

    Implement Prototype Pattern (Shallow Copy)

--- CSharp10DesignPatterns/Starter solution/Prototype/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Prototype/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+Manager manager = new("Ltd. Dan");
+Manager managerClone = (Manager)manager.Clone();
+
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+//Employee employee = new("Forrest G", manager);
+Employee employee = new("Forrest G", managerClone);
+Employee employeeClone = (Employee)employee.Clone();
+Console.WriteLine($"Employee was cloned: {employee.Name}, with manager {employee.Manager.Name}");
+
+// change the manager name
+managerClone.Name = "MegaSpaceKaren";
+Console.WriteLine($"Employee was cloned: {employee.Name}, with manager {employee.Manager.Name}");
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
