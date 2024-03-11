386f743c00732fda6a7e5bf53bae7ce3a4ad2b2a
Brian Andersen <brianmanden@gmail.com>
2024-03-11 12:45:21 +0100

    Implement Bridge Pattern

--- CSharp10DesignPatterns/Starter solution/Bridge/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Bridge/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.Title = "Bridge";
+﻿using Bridge;
+
+Console.Title = "Bridge";
+
+NoCoupon noCoupon = new();
+OneEuroCoupon oneEruoCoupon = new();
+
+MeatBasedMenu meatBasedMenu = new(noCoupon);
+Console.WriteLine($"Meat based menu, no coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+meatBasedMenu = new(oneEruoCoupon);
+Console.WriteLine($"Meat based menu, one euro coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+VegetarianMenu vegetarianMenu = new(noCoupon);
+Console.WriteLine($"Vegetarian menu, no coupon: {vegetarianMenu.CalculatePrice()} euro.");
+
+vegetarianMenu = new(oneEruoCoupon);
+Console.WriteLine($"Vegetarian menu, one euro coupon: {vegetarianMenu.CalculatePrice()} euro.");
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
