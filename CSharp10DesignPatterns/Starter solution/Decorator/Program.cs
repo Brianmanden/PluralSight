14816261c4ced72b6ae81bc96d48eb7036df7371
Brian Andersen <brianmanden@gmail.com>
2024-03-12 08:23:15 +0100

    Implement Decorator Pattern

--- CSharp10DesignPatterns/Starter solution/Decorator/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Decorator/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Decorator";
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+// instantiate mail services
+CloudMailService cloudMailService = new();
+cloudMailService.SendMail("Hello there from cloud :)");
+
+OnPremiseMailService onPremiseMailService = new();
+onPremiseMailService.SendMail("Hello there from premises :)");
+
+// add behavior
+StatisticsDecorator statisticsDecorator = new(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper");
+
+// add behavior
+MessageDatabaseDecorator messageDatabaseDecorator = new(onPremiseMailService);
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 1.");
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 2.");
+
+foreach (string message in messageDatabaseDecorator.SentMessages)
+{
+	Console.WriteLine($"Stored message: \"{message}\"");
+}
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
