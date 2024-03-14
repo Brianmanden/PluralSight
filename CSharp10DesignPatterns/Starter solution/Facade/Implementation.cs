51054fb54e3c919655491068db12409ca78a2665
Brianmanden <brianmanden@gmail.com>
2024-03-14 08:44:37 +0100

    Implement Facade Pattern

--- CSharp10DesignPatterns/Starter solution/Facade/Implementation.cs
+++ CSharp10DesignPatterns/Starter solution/Facade/Implementation.cs
@@ -1,3 +1,63 @@
 ﻿namespace Facade
 {
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class OrderService
+    {
+        public bool HasEnoughOrders(int customerId)
+        {
+            //fake calculation for demo purposes
+            return (customerId > 5);
+        }
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class CustomerDiscountService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            //fake calculation for demo purposes
+            return (customerId > 8) ? 10 : 20;
+        }
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class DayOfTheWeekFactorService
+    {
+        public double CalculateDayOfTheWeekFactor()
+        {
+            switch (DateTime.UtcNow.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    return 0.8;
+                default:
+                    return 1.2;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Facade
+    /// </summary>
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountService _customerDiscountBaseService = new();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscountPercentage(int customerId) {
+            if (!_orderService.HasEnoughOrders(customerId))
+            {
+                return 0;
+            }
+
+            return _customerDiscountBaseService.CalculateDiscountBase(customerId) * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
 }
--- CSharp10DesignPatterns/Starter solution/Facade/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Facade/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.Title = "Facade";
+﻿using Facade;
+
+Console.Title = "Facade";
+
+DiscountFacade facade = new();
+Console.WriteLine($"Discount percentage for customder with id: 1: {facade.CalculateDiscountPercentage(1)}");
+Console.WriteLine($"Discount percentage for customder with id: 10: {facade.CalculateDiscountPercentage(10)}");
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
