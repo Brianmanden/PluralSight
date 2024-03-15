ab2a08256449ca5140a3543952bfb8947e632774
Brian Andersen <brianmanden@gmail.com>
2024-03-15 07:26:00 +0100

    Implement Proxy Pattern

--- CSharp10DesignPatterns/Starter solution/Proxy/Implementation.cs
+++ CSharp10DesignPatterns/Starter solution/Proxy/Implementation.cs
@@ -1,3 +1,66 @@
 ﻿namespace Proxy
 {
+	/// <summary>
+	/// Subject
+	/// </summary>
+	public interface IDocument
+	{
+		void DisplayDocument();
+	}
+
+	/// <summary>
+	/// RealSubject
+	/// </summary>
+	public class Document : IDocument
+	{
+		public string? Title { get; set; }
+		public string? Content { get; set; }
+		public int AuthorId { get; set; }
+		public DateTimeOffset LastAccessed { get; private set; }
+		private string _fileName;
+
+		public Document(string fileName)
+		{
+			_fileName = fileName;
+			LoadDocument(fileName);
+		}
+
+		private void LoadDocument(string fileName)
+		{
+			Console.WriteLine($"Executing expensive action: loading a file (\"{fileName}\") from disk");
+			// fake loading...
+			Thread.Sleep(1000);
+
+			Title = "An expensive document";
+			Content = "Lots and lots of content";
+			AuthorId = 1;
+			LastAccessed = DateTimeOffset.UtcNow;
+		}
+
+		public void DisplayDocument()
+		{
+			Console.WriteLine($"Title: {Title}, Content: {Content}");
+		}
+	}
+
+	public class DocumentProxy : IDocument
+	{
+		private Document? _document;
+		private string _fileName;
+
+		public DocumentProxy(string fileName)
+		{
+			_fileName = fileName;
+		}
+
+		public void DisplayDocument()
+		{
+			if (_document == null)
+			{
+				_document = new(_fileName);
+			}
+
+			_document.DisplayDocument();
+		}
+	}
 }
--- CSharp10DesignPatterns/Starter solution/Proxy/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Proxy/Program.cs
@@ -1,1 +1,20 @@
 ﻿Console.Title = "Proxy";
+
+//without proxy
+Console.WriteLine("Constructing document.");
+Proxy.Document myDocument = new("MyDocument.pdf");
+Console.WriteLine("Document constructed.");
+myDocument.DisplayDocument();
+
+
+Console.WriteLine(Environment.NewLine);
+
+
+Console.WriteLine("Constructing document proxy.");
+Proxy.DocumentProxy myDocumentProxy = new("MyDocument.pdf");
+Console.WriteLine("Document proxy constructed.");
+myDocumentProxy.DisplayDocument();
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
