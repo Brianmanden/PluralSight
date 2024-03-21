54f2b17527ddcbe20e6372f7ab49557e60abf370
Brian Andersen <brianmanden@gmail.com>
2024-03-21 18:41:00 +0100

    Implement Template Pattern

--- CSharp10DesignPatterns/Starter solution/TemplateMethod/Program.cs
+++ CSharp10DesignPatterns/Starter solution/TemplateMethod/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("bf3a298c-9990-4b02-873d-3d3c98ad16d2"));
+Console.WriteLine(Environment.NewLine);
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("07b8a8c7-c598-4b6c-9049-ecce9fe4a56b"));
+Console.WriteLine(Environment.NewLine);
+
+EudoraMailParser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody("789fe935-ced2-4fbd-865b-172909560a93"));
+Console.WriteLine(Environment.NewLine);
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
