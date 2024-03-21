9f9c7042a9ff131be4e85c8b54d3ffb6bd286fa4
Brian Andersen <brianmanden@gmail.com>
2024-03-21 18:41:37 +0100

    Implement Strategy Pattern

--- CSharp10DesignPatterns/Starter solution/Strategy/Implementation.cs
+++ CSharp10DesignPatterns/Starter solution/Strategy/Implementation.cs
@@ -1,3 +1,82 @@
 ﻿namespace Strategy
 {
+	/// <summary>
+	/// Strategy
+	/// </summary>
+	public interface IExportService
+	{
+		public void Export(Order order);
+	}
+
+	/// <summary>
+	/// ConcreteStrategy
+	/// </summary>
+	public class JsonExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+			Console.WriteLine($"Exporting {order.Name} to JSON.");
+		}
+	}
+
+	/// <summary>
+	/// ConcreteStrategy
+	/// </summary>
+	public class XmlExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+			Console.WriteLine($"Exporting {order.Name} to XML.");
+		}
+	}
+
+	/// <summary>
+	/// ConcreteStrategy
+	/// </summary>
+	public class CsvExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+			Console.WriteLine($"Exporting {order.Name} to CSV.");
+		}
+	}
+
+	/// <summary>
+	/// Context
+	/// </summary>
+	public class Order
+	{
+		public string Customer { get; set; }
+		public int Amount { get; set; }
+		public string Name { get; set; }
+		public string? Description { get; set; }
+		//public IExportService ExportService { get; set; }
+
+		public Order(string customer, int amount, string name)
+		//public Order(string customer, int amount, string name, IExportService exportService)
+		{
+			Customer = customer;
+			Amount = amount;
+			Name = name;
+			//ExportService = new CsvExportService();
+			//ExportService = exportService;
+		}
+
+		//public void Export()
+		//{
+		//	ExportService?.Export(this);
+		//}
+
+		public void Export(IExportService exportService)
+		{
+			if (exportService is null)
+			{
+				throw new NullReferenceException(nameof(exportService));
+			}
+
+			exportService.Export(this);
+		}
+	}
+
+
 }
--- CSharp10DesignPatterns/Starter solution/Strategy/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Strategy/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+Order order = new("Marvin Software", 5, "Visual Studio License");
+//order.ExportService = new CsvExportService();
+//order.Export();
+order.Export(new CsvExportService());
+
+//order.ExportService = new JsonExportService();
+//order.Export();
+order.Export(new JsonExportService());
+
+//order.ExportService = new XmlExportService();
+//order.Export();
+order.Export((XmlExportService)new());
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
