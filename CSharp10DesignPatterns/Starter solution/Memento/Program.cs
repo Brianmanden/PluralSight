f6dd4cd00b6fa667d8fed751922889a4f2a3b548
Brian Andersen <brianmanden@gmail.com>
2024-03-21 19:13:36 +0100

    Implement Memento Pattern

--- CSharp10DesignPatterns/Starter solution/Memento/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Memento/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.Title = "Memento";
+﻿using Memento;
+
+Console.Title = "Memento";
+
+CommandManager commandManager = new();
+IEmployeeManagerRepository repository = new EmployeeManagerRepository();
+
+commandManager.Invoke(
+new AddEmployeeToManagerList(repository, 1, new Employee(111, "Alice"))
+);
+repository.WriteDataStore();
+
+
+commandManager.Invoke(
+new AddEmployeeToManagerList(repository, 2, new Employee(112, "Bob"))
+);
+repository.WriteDataStore();
+
+
+commandManager.Undo();
+repository.WriteDataStore();
+
+
+commandManager.Invoke(
+new AddEmployeeToManagerList(repository, 1, new Employee(113, "Charlie"))
+);
+repository.WriteDataStore();
+
+
+// try adding the same employee again
+commandManager.Invoke(
+new AddEmployeeToManagerList(repository, 1, new Employee(113, "Charlie"))
+);
+repository.WriteDataStore();
+
+
+commandManager.UndoAll();
+repository.WriteDataStore();
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
