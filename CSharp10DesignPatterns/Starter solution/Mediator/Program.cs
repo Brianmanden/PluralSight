90f48f139b13f2525f532c4133517eba00920b09
Brian Andersen <brianmanden@gmail.com>
2024-03-22 11:18:13 +0100

    Implement Mediator Pattern

--- CSharp10DesignPatterns/Starter solution/Mediator/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Mediator/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.Title = "Mediator";
+﻿using Mediator;
+Console.Title = "Mediator";
+
+
+TeamChatRoom teamChatRoom = new();
+
+
+TeamMember huey = new Lawyer("Huey");
+TeamMember dewey = new Lawyer("Dewey");
+TeamMember louie = new AccountManager("Louie");
+TeamMember donald = new AccountManager("Donald");
+TeamMember daisy = new AccountManager("Daisy");
+
+
+teamChatRoom.Register(huey);
+teamChatRoom.Register(dewey);
+teamChatRoom.Register(louie);
+teamChatRoom.Register(donald);
+teamChatRoom.Register(daisy);
+
+
+louie.Send("Hi all, can someone check file ABC123? I need a compliance check.");
+huey.Send("On it!");
+huey.Send("Louie", "Let´s talk in a Teams call?");
+huey.Send("Louie", "All good :)");
+louie.SendTo<AccountManager>("The file has been approved!");
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
