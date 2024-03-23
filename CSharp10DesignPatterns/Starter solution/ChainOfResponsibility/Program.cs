72c14aafb99a9d3d67a0643f5e0e4cc6d99e9660
Brian Andersen <brianmanden@gmail.com>
2024-03-23 10:05:52 +0100

    Implement Chain of Responsibility Pattern

--- CSharp10DesignPatterns/Starter solution/ChainOfResponsibility/Program.cs
+++ CSharp10DesignPatterns/Starter solution/ChainOfResponsibility/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using System.ComponentModel.DataAnnotations;
+
+using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+Document validDocument = new("How to avoid BrainFuck development", DateTimeOffset.UtcNow, true, true);
+Document invalidDocument = new("How to avoid BrainFuck development", DateTimeOffset.UtcNow, false, true);
+
+var documentHandlerChain = new Document.DocumentTitleHandler();
+documentHandlerChain
+	.SetSuccessor(new Document.DocumentLastModifiedHandler())
+	.SetSuccessor(new Document.DocumentApprovedByLitigationHandler())
+	.SetSuccessor(new Document.DocumentApprovedByManagementHandler());
+
+try
+{
+	documentHandlerChain.Handle(validDocument);
+	Console.WriteLine("Valid document is valid");
+	documentHandlerChain.Handle(invalidDocument);
+	Console.WriteLine("Invalid document is invalid");
+}
+catch (ValidationException validationException)
+{
+	Console.WriteLine(validationException.Message);
+}
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
