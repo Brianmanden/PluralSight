d1c3e9792f766bf6fd88422cd2984763a381e26d
Brian Andersen <brianmanden@gmail.com>
2024-03-24 10:20:26 +0100

    Implement Observer Pattern

--- CSharp10DesignPatterns/Starter solution/Observer/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Observer/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.Title = "Observer";
+﻿using Observer;
+
+Console.Title = "Observer";
+
+TicketStockService ticketStockService = new();
+TicketResellerService ticketResellerService = new();
+OrderService orderService = new();
+
+// add two observers
+orderService.AddObserver(ticketStockService);
+orderService.AddObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(1, 2);
+
+Console.WriteLine();
+
+// remove observer
+orderService.RemoveObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(2, 3);
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
