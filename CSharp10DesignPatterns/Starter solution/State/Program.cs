316529ad621fb976dee17990490524c4db9f3fbe
Brian Andersen <brianmanden@gmail.com>
2024-03-25 12:02:14 +0100

    Implement State Pattern

--- CSharp10DesignPatterns/Starter solution/State/Program.cs
+++ CSharp10DesignPatterns/Starter solution/State/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.Title = "State";
+﻿using State;
+
+Console.Title = "State";
+
+BankAccount bankAccount = new();
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+bankAccount.Deposit(100);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+bankAccount.Withdraw(500);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+bankAccount.Withdraw(100);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+// deposit enough to go to gold state of account
+bankAccount.Deposit(2000);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+// should be in gold state now
+bankAccount.Deposit(100);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+// back to overdraw
+bankAccount.Withdraw(3000);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+// should transition to regular
+bankAccount.Deposit(3000);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+// should still be in regular
+bankAccount.Deposit(100);
+Console.WriteLine($"Current balance is: {bankAccount.Balance}{Environment.NewLine}");
+
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
