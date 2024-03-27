1cd4c08f8069e598bdbec39e2d30fe2f276a4f61
Brian Andersen <brianmanden@gmail.com>
2024-03-27 08:56:24 +0100

    Implement Visitor Pattern

--- CSharp10DesignPatterns/Starter solution/Visitor/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Visitor/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Title = "Visitor";
+﻿using Visitor;
+
+Console.Title = "Visitor";
+
+// create container and add concrete elements
+Container container = new();
+
+container.Customers.Add(new Customer("Sophie", 500));
+container.Customers.Add(new Customer("Karen", 1000));
+container.Customers.Add(new Customer("Sven", 800));
+container.Employees.Add(new Employee("Kevin", 18));
+container.Employees.Add(new Employee("Tom", 5));
+
+// create visitor
+DiscountVisitor discountVisitor = new();
+
+// pass it through to container
+container.Accept(discountVisitor);
+
+// write out gathered amount
+Console.WriteLine($"Total discount: {discountVisitor.TotalDiscountGiven}");
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
