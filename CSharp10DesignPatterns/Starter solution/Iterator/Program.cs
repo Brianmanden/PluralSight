517edb713644dd44eb852f43766def1065962b43
Brian Andersen <brianmanden@gmail.com>
2024-03-27 08:56:04 +0100

    Implement Iterator Pattern

--- CSharp10DesignPatterns/Starter solution/Iterator/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Iterator/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.Title = "Iterator";
+﻿using Iterator;
+
+Console.Title = "Iterator";
+
+// create the collection
+PeopleCollection people = new();
+people.Add(new Person("Joe", "The Wild West"));
+people.Add(new Person("Jack", "The Wild West"));
+people.Add(new Person("William", "The Wild West"));
+people.Add(new Person("Averell", "The Wild West"));
+
+// create the iterator
+IPeopleIterator peopleIterator = people.CreateIterator();
+
+// use the iterator to parse through the people
+// in the collection, they should be shown in alphabetical order
+for (Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+{
+	Console.WriteLine(person?.Name);
+}
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
