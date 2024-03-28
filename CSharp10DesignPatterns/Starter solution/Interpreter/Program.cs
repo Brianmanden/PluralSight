893ff7def5927db99afb11743e7720f61f1443a0
Brian Andersen <brianmanden@gmail.com>
2024-03-28 10:37:06 +0100

    Implement Interpreter Pattern

--- CSharp10DesignPatterns/Starter solution/Interpreter/Program.cs
+++ CSharp10DesignPatterns/Starter solution/Interpreter/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.Title = "Interpreter";
+﻿using Interpreter;
+
+Console.Title = "Interpreter";
+
+List<RomanExpression> expressions = new()
+{
+	new RomanHundredExpression(),
+	new RomanTenExpression(),
+	new RomanOneExpression(),
+};
+
+
+//RomanContext context = new(5);
+//RomanContext context = new(49);
+//RomanContext context = new(133);
+//RomanContext context = new(181);
+RomanContext context = new(987);
+foreach (RomanExpression expression in expressions)
+{
+	expression.Interpret(context);
+}
+
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: {context.PristineInput} = {context.Output}");
+
+Console.WriteLine(Environment.NewLine + "Press any key to terminate program.");
+Console.ReadKey();
