8cc9b396189ff418319908e824b592c4a797c538
Brian Andersen <brianmanden@gmail.com>
2024-03-30 08:50:17 +0100

    CHANGE HomeController to async

--- c-sharp-asynchronous-programming/02/Start_Here/StockAnalyzer.Web/Controllers/HomeController.cs
+++ c-sharp-asynchronous-programming/02/Start_Here/StockAnalyzer.Web/Controllers/HomeController.cs
@@ -1,28 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
 using Newtonsoft.Json;
+
 using StockAnalyzer.Core.Domain;
 using StockAnalyzer.Web.Models;
-using System.Diagnostics;
 
 namespace StockAnalyzer.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
+	private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+	public async Task<ActionResult> Index()
+	{
+		using (HttpClient client = new())
+		{
+			// using hadrcoded value to simplify demo
+			Task<HttpResponseMessage> responseTask = client.GetAsync($"{API_URL}/CAT");
+			HttpResponseMessage response = await responseTask;
+			string content = await response.Content.ReadAsStringAsync();
+			IEnumerable<StockPrice> data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content)!;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+			return View(data);
+		}
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
