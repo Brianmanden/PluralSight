ef51759ecb547d996b017b0520c501875c82a357
Brian Andersen <brianmanden@gmail.com>
2024-03-30 08:09:04 +0100

    REFACTOR Windows app method to async

--- c-sharp-asynchronous-programming/02/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ c-sharp-asynchronous-programming/02/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,73 +1,69 @@
-﻿using Newtonsoft.Json;
-using StockAnalyzer.Core.Domain;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
-using System.Threading;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
+
+using Newtonsoft.Json;
+
+using StockAnalyzer.Core.Domain;
 
 namespace StockAnalyzer.Windows;
 
 public partial class MainWindow : Window
 {
-    private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
-    private Stopwatch stopwatch = new Stopwatch();
+	private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
+	private Stopwatch stopwatch = new Stopwatch();
 
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
-
-
-
-    private void Search_Click(object sender, RoutedEventArgs e)
-    {
-        BeforeLoadingStockData();
-
-        var client = new WebClient();
-
-        var content = client.DownloadString($"{API_URL}/{StockIdentifier.Text}");
-
-        // Simulate that the web call takes a very long time
-        Thread.Sleep(10000);
-
-        var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-        Stocks.ItemsSource = data;
-
-        AfterLoadingStockData();
-    }
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
 
 
 
+	private async void Search_Click(object sender, RoutedEventArgs e)
+	{
+		BeforeLoadingStockData();
+
+		using (HttpClient client = new())
+		{
+			Task<HttpResponseMessage> responseTask = client.GetAsync($"{API_URL}/{StockIdentifier.Text}");
+			HttpResponseMessage response = await responseTask;
+			string content = await response.Content.ReadAsStringAsync();
+
+			IEnumerable<StockPrice> data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content)!;
+			Stocks.ItemsSource = data;
+		}
+
+		AfterLoadingStockData();
+	}
 
 
 
+	private void BeforeLoadingStockData()
+	{
+		stopwatch.Restart();
+		StockProgress.Visibility = Visibility.Visible;
+		StockProgress.IsIndeterminate = true;
+	}
 
+	private void AfterLoadingStockData()
+	{
+		StocksStatus.Text = $"Loaded stocks for {StockIdentifier.Text} in {stopwatch.ElapsedMilliseconds}ms";
+		StockProgress.Visibility = Visibility.Hidden;
+	}
 
-    private void BeforeLoadingStockData()
-    {
-        stopwatch.Restart();
-        StockProgress.Visibility = Visibility.Visible;
-        StockProgress.IsIndeterminate = true;
-    }
+	private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+	{
+		Process.Start(new ProcessStartInfo { FileName = e.Uri.AbsoluteUri, UseShellExecute = true });
 
-    private void AfterLoadingStockData()
-    {
-        StocksStatus.Text = $"Loaded stocks for {StockIdentifier.Text} in {stopwatch.ElapsedMilliseconds}ms";
-        StockProgress.Visibility = Visibility.Hidden;
-    }
+		e.Handled = true;
+	}
 
-    private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
-    {
-        Process.Start(new ProcessStartInfo { FileName = e.Uri.AbsoluteUri, UseShellExecute = true });
-
-        e.Handled = true;
-    }
-
-    private void Close_OnClick(object sender, RoutedEventArgs e)
-    {
-        Application.Current.Shutdown();
-    }
+	private void Close_OnClick(object sender, RoutedEventArgs e)
+	{
+		Application.Current.Shutdown();
+	}
 }
